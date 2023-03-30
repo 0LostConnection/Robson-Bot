0abf7dec3d931ff4d06708ad1883ea05157f2f05
Geovane <saraivasilva75@gmail.com>
2023-03-30 16:30:34 -0300

    Gonna start working on integrating a database

--- src/database/models/Guild.js
+++ src/database/models/Guild.js
@@ -2,9 +2,6 @@ const { Schema, model } = require('mongoose')
 
 const guildSchema = new Schema({
     _id: String,
-    vips: [
-        Object
-    ],
 })
 
 module.exports = model('guilds', guildSchema)

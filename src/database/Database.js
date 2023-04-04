56c71301b2c956bcaeb3b23fd0498bc64817da01
Geovane <saraivasilva75@gmail.com>
2023-04-04 15:54:51 -0300

    Some changes

--- src/database/Database.js
+++ src/database/Database.js
@@ -1,20 +1,29 @@
 module.exports = async (guildId) => {
     if (!guildId) return console.log('Provide and ID!')
-    const { connect, set } = require('mongoose')
+    const { connect, set, connection, disconnect } = require('mongoose')
     const Models = require('./Models')
-    
+
     set("strictQuery", true)
-    const connection = await connect(process.env.DATABASE_SECRET)
+    const databaseConnection = await connect(process.env.DATABASE_SECRET, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => console.log('\x1b[32m%s\x1b[0m', 'Banco de dados conectado com sucesso!'))
+        .catch(err => console.error('Erro ao conectar com MongoDB: ' + err));
 
-    //console.log('\x1b[32m%s\x1b[0m', 'Banco de dados conectado com sucesso!')
-
-    const database = { connection, ...Models }
+    const database = { databaseConnection, ...Models }
     return {
+        //client: await database.client 
         guild: await database.guilds.findById(guildId) || new database.guilds({ _id: guildId }),
         disconnect: () => {
-            connection.disconnect().then(() => {
-                //console.log('\x1b[32m%s\x1b[0m', 'Conexão com o banco de dados encerrada!')
-            })
+            connection.close(function (err) {
+                if (err) {
+                    console.error('Erro ao desconectar do banco de dados: ' + err);
+                } else {
+                    disconnect()
+                    console.log('\x1b[32m%s\x1b[0m', 'Desconexão do banco de dados bem-sucedida!');
+                }
+            });
         }
     }
 }
